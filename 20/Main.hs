933dce1e4799628fb685b9ec8b447597c306dabb
Dreary Lisper <drearylisper@gmail.com>
2021-12-21 10:39:55 +0000

    Code Format

--- 20/Main.hs
+++ 20/Main.hs
@@ -8,7 +8,8 @@ type Point = (Int, Int)
 data Image = Image Point Point (Map.Map Point Int) Int deriving Show
 
 parseInput :: String -> ([Int], Image)
-parseInput c = let l = lines c in (parsePixels $ head l, Image (-1, -1) (length (drop 2 l), length (l !! 2)) (parseImage $ drop 2 l) 0)
+parseInput c = let l = lines c in (parsePixels $ head l,
+                                   Image (-1, -1) (length (drop 2 l), length (l !! 2)) (parseImage $ drop 2 l) 0)
   where
     parseImage l = Map.fromList [((i, j), if e == '#' then 1 else 0) | (i, row) <- zip [0..] l, (j, e) <- zip [0..] row]
     parsePixels l = map (\e -> if e == '#' then 1 else 0) l
@@ -36,8 +37,14 @@ part1 c = length $ filter (==1) $ pixels $ iterate step image !! 2
     step :: Image -> Image
     step (Image (x1, y1) (x2, y2) m dflt) = Image (x1-2, y1-2) (x2+2, y2+2) newM newDflt
       where
-        newM = Map.fromList (map (\(point, pixels) -> (point, outputPixels !! bin2int pixels)) $ patches $ Image (x1, y1) (x2, y2) m dflt)
-        newDflt = if dflt == 0 then outputPixels !! 0 else outputPixels !! bin2int [1, 1, 1, 1, 1, 1, 1, 1, 1]
+        newM = Map.fromList
+          (map (\(point, pixels) -> (point, outputPixels !! bin2int pixels))
+           $ patches
+           $ Image (x1, y1) (x2, y2) m dflt)
+
+        newDflt = if dflt == 0
+          then outputPixels !! 0
+          else outputPixels !! bin2int [1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 part2 :: String -> Int
 part2 c = length $ filter (==1) $ pixels $ iterate step image !! 50
@@ -47,8 +54,13 @@ part2 c = length $ filter (==1) $ pixels $ iterate step image !! 50
     step :: Image -> Image
     step (Image (x1, y1) (x2, y2) m dflt) = Image (x1-2, y1-2) (x2+2, y2+2) newM newDflt
       where
-        newM = Map.fromList (map (\(point, pixels) -> (point, outputPixels !! bin2int pixels)) $ patches $ Image (x1, y1) (x2, y2) m dflt)
-        newDflt = if dflt == 0 then outputPixels !! 0 else outputPixels !! bin2int [1, 1, 1, 1, 1, 1, 1, 1, 1]
+        newM = Map.fromList
+          (map (\(point, pixels) -> (point, outputPixels !! bin2int pixels))
+           $ patches
+           $ Image (x1, y1) (x2, y2) m dflt)
+        newDflt = if dflt == 0
+          then outputPixels !! 0
+          else outputPixels !! bin2int [1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 solve filename = do
   c <- readFile filename
